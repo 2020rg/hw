fc6717bc98f1fde132a256f54807f4994c800c7e
Luv-z <714705217@qq.com>
2020-05-10 17:02:50 +0800

    添加session依赖

--- src/main/java/com/ssrg/hw/HwApplication.java
+++ src/main/java/com/ssrg/hw/HwApplication.java
@@ -3,17 +3,69 @@ package com.ssrg.hw;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.server.ConfigurableWebServerFactory;
+import org.springframework.boot.web.server.WebServerFactoryCustomizer;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.freemarker.FreeMarkerViewResolver;
 
 @SpringBootApplication
-public class HwApplication  extends SpringBootServletInitializer {
+@Configuration
+public class HwApplication{
 
     public static void main(String[] args) {
         SpringApplication.run(HwApplication.class, args);
     }
 
-    @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-        return application.sources(HwApplication.class);
+
+    /**
+     * session时长设置
+     * @return
+     */
+    @Bean
+    public WebServerFactoryCustomizer<ConfigurableWebServerFactory> webServerFactoryCustomizer(){
+        return new WebServerFactoryCustomizer<ConfigurableWebServerFactory>() {
+            @Override
+            public void customize(ConfigurableWebServerFactory factory) {
+                factory.setPort(8080);
+            }
+        };
     }
+
+    /**
+     * 添加对jsp支持
+     *
+     */
+/*
+    @Bean
+    public ViewResolver getJspViewResolver() {
+        InternalResourceViewResolver internalResourceViewResolver = new InternalResourceViewResolver();
+        internalResourceViewResolver.setPrefix("/");//前缀
+        internalResourceViewResolver.setSuffix(".jsp");//后缀
+        internalResourceViewResolver.setOrder(0);//优先级
+        return internalResourceViewResolver;
+    }
+*/
+    /**
+     * 添加对Freemarker支持
+     *
+     */
+/*
+    @Bean
+    public FreeMarkerViewResolver getFreeMarkerViewResolver() {
+        FreeMarkerViewResolver freeMarkerViewResolver = new FreeMarkerViewResolver();
+        freeMarkerViewResolver.setCache(false);
+        freeMarkerViewResolver.setPrefix("/pages/");//前缀
+        freeMarkerViewResolver.setSuffix(".html");//后缀
+        freeMarkerViewResolver.setRequestContextAttribute("request");
+        freeMarkerViewResolver.setOrder(1);//优先级
+        freeMarkerViewResolver.setContentType("text/html;charset=UTF-8");
+        return freeMarkerViewResolver;
+
+    }
+    */
 }
--- src/main/java/com/ssrg/hw/controller/LoginController.java
+++ src/main/java/com/ssrg/hw/controller/LoginController.java
@@ -2,8 +2,10 @@ package com.ssrg.hw.controller;
 
 import com.ssrg.hw.dto.StudentDto;
 import com.ssrg.hw.service.IStudentService;
+import com.ssrg.hw.service.ITeacherService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
@@ -11,34 +13,47 @@ import java.util.HashMap;
 import java.util.Map;
 
 @RestController
+@RequestMapping("/login")
 public class LoginController {
 
     @Autowired
     private IStudentService studentService;
 
-    @RequestMapping("/studentLogin")
-    public Map<String, Object> studentLogin(int phone, String password, HttpServletRequest request){
+    @Autowired
+    private ITeacherService teacherService;
+
+    @RequestMapping("/userLogin")
+    public Map<String, Object> studentLogin(@RequestParam("phone") int phone, @RequestParam("password") String password, @RequestParam("role") String role, HttpServletRequest request){
         Map<String, Object> map = new HashMap<String, Object>();
-        Integer flag = null;
-        StudentDto studentDto = studentService.queryStudentByPhone(phone);
-        if (studentDto != null) {
-            if (password.equals(studentDto.getPassword())) {
-                flag = 0;
-                //登陆成功
-                map.put("studentId", studentDto.getStudentId());
-                request.getSession().setAttribute("studentId", studentDto.getStudentId());
-                request.getSession().setAttribute("name", studentDto.getName());
-                request.getSession().setAttribute("school", studentDto.getSchool());
-                request.getSession().setAttribute("phone", studentDto.getPhone());
-                request.getSession().setAttribute("enterSchoolYear", studentDto.getEnterSchoolYear());
+        Integer flag = 1;
+
+        if(role.equals("student")){
+            StudentDto studentDto = studentService.queryStudentByPhone(phone);
+            if (studentDto != null) {
+                if (password.equals(studentDto.getPassword())) {
+                    flag = 0;
+                    //登陆成功
+                    map.put("studentId", studentDto.getStudentId());
+
+                    request.getSession().setAttribute("studentId", studentDto.getStudentId());
+                    request.getSession().setAttribute("name", studentDto.getName());
+                    request.getSession().setAttribute("school", studentDto.getSchool());
+                    request.getSession().setAttribute("phone", studentDto.getPhone());
+                    request.getSession().setAttribute("enterSchoolYear", studentDto.getEnterSchoolYear());
+
+                } else {
+                    flag = 1;
+                    //登录密码不正确
+                }
             } else {
-                flag = 1;
-                //登录密码不正确
+                flag = 2;
+                //此账号不存在
             }
-        } else {
-            flag = 2;
-            //此账号不存在
         }
+        else if(role.equals("teacher")){
+
+        }
+
         map.put("flag", flag);
         return map;
     }
